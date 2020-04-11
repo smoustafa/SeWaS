c7477aab69d843bb26350d73adecb21a1d3f484d
Salli Moustafa <moustafasalli@gmail.com>
2020-04-11 21:17:09 +0200

    Do not rely on an external vcpkg
    We could be tempted to use a vcpkg found globally on the system for reducing the build time.
    However, that vcpkg could not be up-to-date or its portfiles had been modified by the owner.
    To avoid that, and to have full control on the dependencies we are installing, we prefer to only
    check if vcpkg exists on our local installation path.

--- scripts/bootstrap-sewas.ps1
+++ scripts/bootstrap-sewas.ps1
@@ -5,7 +5,7 @@ $VCPKG_BUILD="$SEWAS_ROOT/thirdparty/vcpkg/build"
 
 Write-Output "[START] Bootstraping"
 
-$_vcpkg_not_found=!@("(Get-Command vcpkg.exe -ErrorAction SilentlyContinue)" -or (Test-Path "$VCPKG_ROOT/vcpkg.exe"))
+$_vcpkg_not_found=!@(Test-Path "$VCPKG_ROOT/vcpkg.exe")
 if ($_vcpkg_not_found)
 {
     Write-Output "vcpkg has not been found"
@@ -33,7 +33,7 @@ if ($_vcpkg_not_found)
 }
 else
 {
-    If (Test-Path "$VCPKG_ROOT/vcpkg.exe") {$_vcpkg="$VCPKG_ROOT/vcpkg.exe"} Else {$_vcpkg=(Get-Command vcpkg.exe).Source}
+    $_vcpkg="$VCPKG_ROOT/vcpkg.exe"
     
     Write-Output "vcpkg found : $_vcpkg"
     Write-Output "vcpkg version :"
